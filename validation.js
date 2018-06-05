46b322ed079de37bc7f1f0b3c336867d2f0a3a55
princeppt05 <39814957+princeppt05@users.noreply.github.com>
2018-06-05 00:34:27 -0400

    validation.js
    Update :
    Accessing local storage array and retrieving it into a table

--- validation.js
+++ validation.js
@@ -1,12 +1,19 @@
 
 
 var save = [];
-if(localStorage.a_save) {
-	save = JSON.parse(localStorage.a_save);
-}
+				if(localStorage.a_save)
+				{
+				save = JSON.parse(localStorage.a_save);
+				}
 
-
-
+/* var gender = "";
+    if (male.checked == true) {
+        gender = document.getElementById('gender1').value;
+    }
+    else if (female.checked == true) {
+        gender = document.getElementById('gender2').value;
+    }
+									*/
 
 document.getElementById('btn').addEventListener('click',function() 
 									{
@@ -44,6 +51,12 @@ document.getElementById('btn').addEventListener('click',function()
 														{
 															document.getElementById("emailV").innerHTML = "Email cannot be empty";
 														}
+														 // if (male.checked == false && female.checked == false) {
+       								// 						 document.getElementById('select').insertAdjacentHTML('afterend', '<p style="color:red;" id="generror">You must select your gender!</p>');
+       								// 						}
+       								
+
+
 														// if ((user.email.value).indexOf('@') >= 0 && em.indexOf('.com') > em.indexOf('@')))
 														// {
 														// 	document.getElementById("emailV1").innerHTML = "Invalid Email";
@@ -52,8 +65,40 @@ document.getElementById('btn').addEventListener('click',function()
 														// {
 														// 	document.getElementById("emailV1").innerHTML = "Invalid Email";
 														// }
+													
+														
+															table();
 
 									});
+
+
+
+function table() {
+    console.log(localStorage.a_save);
+    if(localStorage.a_save)
+    {
+    var resp = JSON.parse(localStorage.a_save);
+    var len = resp.length;
+    html = '';
+    html = '<table border="1" style="border-collapse:collapse;" id="result1"><tr><td>Username</td><td>Password</td><td>Email</td><td>Location</td><td>Company</td><td>Gender</td></tr>';
+    for (var i = 0; i < len; i++) {
+        html += '<tr>';
+        html += '<td>' + resp[i].username + '</td>';
+        html += '<td>' + resp[i].password + '</td>';
+        html += '<td>' + resp[i].email+ '</td>';
+        html += '<td>' + resp[i].location + '</td>';
+        html += '<td>' + resp[i].company+ '</td>';
+        html += '<td>' + resp[i].genderm + '</td>';
+        html += '<td>' + resp[i].genderf + '</td>';
+        html += '</tr>';
+    }
+    document.getElementById('result').insertAdjacentHTML('afterend', html);
+}
+}
+
+
+
+
 // document.getElementById('btn').addEventListener('click',function() {
 
 // if(document.getElementById('uname').value=="")
