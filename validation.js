fd19ab48b52c82065b6a754574a9e82925029d6c
princeppt05 <39814957+princeppt05@users.noreply.github.com>
2018-06-06 19:36:04 -0400

    validation.js
    dynamically displaying result in table

--- validation.js
+++ validation.js
@@ -1,42 +1,13 @@
 
 
 var save = [];
-				// if(localStorage.a_save)
-				// {
-				// save = JSON.parse(localStorage.a_save);
-				// // table();
-				// }
+				if(localStorage.a_save)
+				{
+				save = JSON.parse(localStorage.a_save);
+				// table();
+				table();
+				}
 
-/* var gender = "";
-    if (male.checked == true) {
-        gender = document.getElementById('gender1').value;
-    }
-    else if (female.checked == true) {
-        gender = document.getElementById('gender2').value;
-    }
-		
-
-
-							*/
-
-
-// function maleg(){
-// 	if (document.getElementById("male").checked = true)
-// {
-// 	document.getElementById('male').value = "male";
-// }
-// else{
-// 	document.getElementById('male').value = "";
-// 	}
-// }
-// if (document.getElementById("female").checked = true)
-// 	{
-// 	document.getElementById('female').value = "female";
-// 	}
-// else{
-// 	document.getElementById('female').value = "";
-// 	}
-// }
 
 document.getElementById('btn').addEventListener('click',function() 
 									{
@@ -50,16 +21,16 @@ document.getElementById('btn').addEventListener('click',function()
 														'email':document.getElementById('email').value,
 														'location':document.getElementById('lctn').value,
 														'company':document.getElementById('cmpy').value,
-														'gender':document.querySelector('input[name="gender"]:checked').value
+														'gender':(document.querySelector('input[name="gender"]:checked')) ? document.querySelector('input[name="gender"]:checked').value : ''
 														
 
 													};
 													//var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-														if((user.username)  && (user.password) && (user.email) !="" )
+														if((user.username)  && (user.password) && (user.email) !=""  )
 														{
 															save.push(user);
 													localStorage.a_save = JSON.stringify(save);
-													table();
+													//table();
 														
 														}
 														if(user.username=="")
@@ -74,9 +45,20 @@ document.getElementById('btn').addEventListener('click',function()
 														}
 														if(user.email=="")
 														{
-															document.getElementById("emailV").innerHTML = "Email cannot be empty";
+															document.getElementById("emailV").innerHTML = "Email Invalid";
 														}
-														
+														// else((/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email)))
+														// {
+														// 	document.getElementById("emailV").innerHTML = "Email Invalid";
+														// }
+														/*if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email.value))
+ 															 {
+																    return (true)
+																  }
+																   { document.getElementById("emailV").innerHTML = "Invalid Email";
+																    return (false)
+																}*/
+
 														 // if (male.checked == false && female.checked == false) {
        								// 						 document.getElementById('select').insertAdjacentHTML('afterend', '<p style="color:red;" id="generror">You must select your gender!</p>');
        								// 						}
@@ -93,15 +75,21 @@ document.getElementById('btn').addEventListener('click',function()
 														// }
 													
 														myFunction();
+														table();
 														
 															
 															
 									});
 
 
+
 function myFunction() {
     document.getElementById("myForm").reset();
 }
+
+/*function table{
+	document.getElementById("result").reset();
+}*/
 
 function table() {
     //console.log(localStorage.a_save);
@@ -110,18 +98,18 @@ function table() {
     var resp = JSON.parse(localStorage.a_save);
     var len = resp.length;
     html = '';
-    html += '<table border="1" style="border-collapse:collapse;" id="result1"><tr><td>Username</td><td>Password</td><td>Email</td><td>Location</td><td>Company</td><td>Gender</td></tr>';
+    html += '<table  ><tr ><td>Username</td><td>Password</td><td >Email</td><td>Location</td><td>Company</td><td>Gender</td></tr>';
     for (var i = 0; i < len; i++) {
-        html += '<tr>';
+        html += '<tr >';
         html += '<td>' + resp[i].username + '</td>';
         html += '<td>' + resp[i].password + '</td>';
-        html += '<td>' + resp[i].email+ '</td>';
+        html += '<td >' + resp[i].email+ '</td>';
         html += '<td>' + resp[i].location + '</td>';
         html += '<td>' + resp[i].company+ '</td>';
         html += '<td>' + resp[i].gender + '</td>';
                 html += '</tr>';
     }
-    document.getElementById('result').insertAdjacentHTML('afterend', html);
+    document.getElementById('result').innerHTML = html;
 }
 }
 
